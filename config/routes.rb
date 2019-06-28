28c52b0b23835524ea82ad8480f19153f7fb60bc
Matthew Banbury <mbanbury@gmail.com>
2019-06-28 13:10:49 -0600

    Fix up starter products

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers
-      resources :products, only: [:index, :show] do
-      end
+      resources :products, only: [:index, :show]
     end
   end
 end
