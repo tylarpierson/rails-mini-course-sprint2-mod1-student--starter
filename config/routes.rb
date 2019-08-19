8c93c8ef25f9c657baa86485c5834d9024d93bd6
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-19 18:03:16 -0400

    fixed routes to add proper nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :customers
-      resources :products, only: [:index, :show]
-      resources :orders, only [:index, :create, :show, :ship]
+      get "/orders/:id/ship", to: "orders#ship"
+      resources :customers do
+        resources :orders, only: [:index, :create]
+      end
+      resources :products, only: [:index, :show] 
+      resources :orders, only [:index, :create] do 
+        resources :products, only: [:index, :create]
+      end
     end
   end
 end
